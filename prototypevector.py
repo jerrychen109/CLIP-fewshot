b0b31eef3428ead67b08fad2ea9da3340a044ff4
bmehta001 <bmehta001@users.noreply.github.com>
2021-04-26 22:32:26 -0500

    Update prototypevector.py

--- prototypevector.py
+++ prototypevector.py
@@ -4,21 +4,26 @@ import prototype
 
 
 class PrototypeVector():
-    def __init__(self, imageEncodeFunc):
+    def __init__(self, imageEncodeFunc, k=None):
         self.imageEncodeFunc = imageEncodeFunc
         self.labels = []
+        self.labelsToPrototypes = {}
         self.allImages = []
         self.allSTDImages = []
         self.allVectors = []
         self.classVectors = []
+        self.k = k
 
-    def addClass(self, label, filenames):
+    def addPrototype(self, label, filenames):
         '''TODO: create a new class i.e. append something to labels, allImages, allSTDImages, allVectors, classVectors'''
+        # newProto = Prototype(imageEncodeFunc, k=self.k)
+        # newProto.addImages(filenames)
+        # self.labels.append(newProto.getImages())
         pass
 
-    def addClasses(self, labels, allFilenames):
+    def addPrototypes(self, labels, allFilenames):
         for label, filenames in zip(labels, allFilenames):
-            addClass(label, filenames)
+            self.addPrototype(label, filenames=filenames)
             
     def classify(self, images):
         '''TODO: classify list of images'''
