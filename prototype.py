454f156f71f37b941b54771f9bd5c9bceb2dccb3
bmehta001 <bhagirath.mehta.bmehta@gmail.com>
2021-04-28 16:12:52 -0500

    Refactor all files, make filenames/images poss

--- prototype.py
+++ prototype.py
@@ -1,55 +1,72 @@
 import utils
 import numpy as np
 import torch
-from utils import *
+from utils.text_utils import *
+from utils.image_utils import standardize, encodeImageWithFunc, normalize, getImagesFromFiles
+
 
 class Prototype():
-    def __init__(self, imageEncodeFunc, seed=1729, k=None, label=None):
+    def __init__(self, imageEncodeFunc, device, label=None, k=None, seed=1729):
         self.imageEncodeFunc = imageEncodeFunc
-        self.seed = seed
-        self.k = k
+        self.device = device
         self.label = label
-        self.images = []
-        self.STDImages = None # tensor
-        self.vectors = None # non-normalized, tensor
-        self.classVectors = None # normalized k vectors, numpy array
-        self.classVector = None # average of k vectors, numpy array
+        self.k = k
+        self.seed = seed
+        self.rng = np.random.default_rng(seed)
+        self.images = None
+        self.STDImages = None  # tensor
+        self.vectors = None  # non-normalized, tensor
+        self.norm_vectors = None
+        self.classVector = None  # average of k vectors, numpy array
 
     def addLabel(self, label):
         self.label = label
 
     def changeLabel(self, label):
         self.addLabel(label)
-        
+
     def changeK(self, k):
         self.k = k
-        
+
     def changeSeed(self, seed):
         self.seed = seed
 
-    def addImage(self, image):
-        # Standardizes and encodes image, updates data structures
-        self.images.append(image)
-        image = [image]
-        STDImage = standardize(image)
-        self.addSTDImage(STDImage)
-        image_vector = self.imageEncodeFunc(STDImage).float()
-        self.addVector(image_vector)
+    # def addImage(self, image):
+    #     # Standardizes and encodes image, updates data structures
+    #     self.images.append(image)
+    #     image = [image]
+    #     STDImage = standardize(image)
+    #     self.addSTDImage(STDImage)
+    #     image_vector = self.imageEncodeFunc(STDImage).float()
+    #     self.addVector(image_vector)
+    def addImagesWithFilenames(self, startDir, filenames):
+        images = getImagesFromFiles(startDir, filenames)
+        self.addImages(images)
 
     def addImages(self, images):
-        self.images.append(images)
-
-    def addSTDImage(self, STDImage):
-        if self.STDImages is None:
-            self.STDImages = STDImage
+        device = self.device
+        if self.images is None:
+            self.images = torch.tensor(np.stack(images)).device()
         else:
-            self.STDImages = torch.cat((self.STDImages, STDImage),0)
+            self.images = torch.tensor(np.stack(self.images, images)).device()
+        self.STDImages = standardize(self.images)
+        self.vectors = encodeImageWithFunc(
+            self.imageEncodeFunc, self.STDImages)
+        self.norm_vectors = normalize(self.vectors)
+        self.classVector = self.rng.choice(norm_vectors, self.k).mean(axis=0)
+        # self.images.append(images)
 
-    def addVector(self, image_vector):
-        if self.vectors is None:
-            self.vectors = image_vector
-        else:
-            self.vectors = torch.cat((self.vectors, image_vector),0)
+    # def addSTDImage(self, STDImage):
+    #     if self.STDImages is None:
+    #         self.STDImages = STDImage
+    #     else:
+    #         self.STDImages = torch.cat((self.STDImages, STDImage), 0)
+
+    # def addVector(self, image_vector):
+    #     if self.vectors is None:
+    #         self.vectors = image_vector
+    #     else:
+    #         self.vectors = torch.cat((self.vectors, image_vector), 0)
 
     def getImages(self):
         return self.images
@@ -59,23 +76,20 @@ class Prototype():
 
     def getVectors(self):
         return self.vectors
-    
-    def getClassVectors(self):
-        return self.classVectors
-    
-    def setClassVectors(self, k=None):
-        # Normalizes random k vectors and sets classVectors
-        if k is None:
-            k = self.k
-        random.seed(self.seed)
-        indices = random.sample(range(len(images)), k)
-        indices = torch.tensor(indices)
-        self.classVectors = normalize(self.vectors[indices]).numpy()
-    
-    def getClassVector(self):
+
+    def getNormVectors(self):
+        return self.norm_vectors
+
+    # def setClassVectors(self, k=None):
+    #     # Normalizes random k vectors and sets classVectors
+    #     if k is None:
+    #         k = self.k
+    #     indices = self.rng(range(len(images)), k)
+    #     indices = torch.tensor(indices)
+    #     self.classVectors = normalize(self.vectors[indices]).numpy()
+
+    def getClassVector(self, k=None):
         # Calculates and returns classVector
-        self.classVector = self.classVectors.mean(axis=0)
-        return self.classVector
-        
-        
-        
+        if k is None or k == self.k:
+            return self.classVector
+        return self.rng.choice(self.norm_vectors, k).mean(axis=0)
--- prototypevector.py
+++ prototypevector.py
@@ -1,37 +1,42 @@
 import utils
 import numpy as np
-import prototype
+from prototype import Prototype
 
 
 class PrototypeVector():
-    def __init__(self, imageEncodeFunc, k=None):
+    def __init__(self, imageEncodeFunc, device, k=None, seed=1729):
         self.imageEncodeFunc = imageEncodeFunc
-        self.labels = []
+        self.device = device
+        self.k = k
+        self.seed = seed
+        self.rng = np.random.default_rng(seed)
         self.labelsToPrototypes = {}
         self.allImages = []
         self.allSTDImages = []
         self.allVectors = []
-        self.classVectors = []
-        self.k = k
+        self.allNormVectors = []
+        self.allClassVectors = []
 
-    def addPrototype(self, label, filenames):
-        newProto = Prototype(self.imageEncodeFunc, k=self.k)
-        newProto.addLabel(label)
-        self.labels.append(label)
-        newProto.addImages(filenames)
-        self.allImages.append(newProto.getImages())
-        for image in newProto.getImages():
-            newProto.addSTDImage(image)
-        self.allSTDImages.append(newProto.getSTDImages())
-        self.allVectors.append(newProto.getVectors())
-        newProto.setClassVectors(self, k=self.k)
-        self.classVectors.append(newProto.getClassVector())
-        self.labelsToPrototypes[label] = newProto
+    def addPrototypeWithFilenames(self, startdir, filenames, label):
+        newPrototype = Prototype(self.imageEncodeFunc,
+                                 self.device, label, self.k, self.seed)
+        newPrototype.addImagesWithFilenames(startdir, filenames)
+        self.allImages.append(newPrototype.getImages())
+        self.allSTDImages.append(newPrototype.getSTDImages())
+        self.allVectors.append(newPrototype.getVectors())
+        self.allNormVectors.append(newPrototype.getNormVectors())
+        self.allClassVectors.append(newPrototype.getClassVector(self.k))
+        self.labelsToPrototypes[label] = newPrototype
 
-    def addPrototypes(self, labels, allFilenames):
-        for label, filenames in zip(labels, allFilenames):
-            self.addPrototype(label, filenames=filenames)
-            
+    def addPrototypesWithFilenames(self, allStartdirs, allFilenames, labels):
+        for startdir, filenames, label in zip(allStartdirs, allFilenames, labels):
+            self.addPrototype(startdir, filenames, label)
+
+    def getClassVectors(self, k=None):
+        if k is None or k == self.k:
+            return self.allClassVectors
+        return [key.getClassVector() for key in self.labelsToPrototypes.keys()]
+
     def classify(self, images):
         '''TODO: classify list of images'''
         pass
