007db3567ad08564fffe205e53bfb74330f167b8
zachchen0 <zacharychen0@gmail.com>
2021-04-27 23:29:37 -0700

    Updated prototype.py
    Added and implemented new variables and methods

--- prototype.py
+++ prototype.py
@@ -1,5 +1,7 @@
 import utils
 import numpy as np
+import torch
+from utils import *
 
 class Prototype():
     def __init__(self, imageEncodeFunc, seed=1729, k=None, label=None):
@@ -8,31 +10,46 @@ class Prototype():
         self.k = k
         self.label = label
         self.images = []
-        self.STDImages = []
-        self.vectors = []
-        self.classVector = []
+        self.STDImages = None # tensor
+        self.vectors = None # non-normalized, tensor
+        self.classVectors = None # normalized k vectors, numpy array
+        self.classVector = None # average of k vectors, numpy array
 
     def addLabel(self, label):
         self.label = label
 
     def changeLabel(self, label):
         self.addLabel(label)
+        
+    def changeK(self, k):
+        self.k = k
+        
+    def changeSeed(self, seed):
+        self.seed = seed
 
-    def addImage(self, filename):
-        ''' TODO: Update self.images, self.STDimages, self.vectors '''
-        pass
+    def addImage(self, image):
+        # Standardizes and encodes image, updates data structures
+        self.images.append(image)
+        image = [image]
+        STDImage = standardize(image)
+        self.addSTDImage(STDImage)
+        image_vector = self.imageEncodeFunc(STDImage).float()
+        self.addVector(image_vector)
 
-    def addImages(self, filenames):
-        for filename in filenames:
-            addImage(filename)
+    def addImages(self, images):
+        self.images.append(images)
 
-    def addSTDImage(self, image):
-        ''' TODO: Add standardized image to STD images '''
-        pass
+    def addSTDImage(self, STDImage):
+        if self.STDImages is None:
+            self.STDImages = STDImage
+        else:
+            self.STDImages = torch.cat((self.STDImages, STDImage),0)
 
-    def addVector(self, image):
-        '''TODO: Do something with the vector (add to vectors + maybe use it to find average?) '''
-        image_vector = self.imageEncodeFunc(image)
+    def addVector(self, image_vector):
+        if self.vectors is None:
+            self.vectors = image_vector
+        else:
+            self.vectors = torch.cat((self.vectors, image_vector),0)
 
     def getImages(self):
         return self.images
@@ -43,7 +60,22 @@ class Prototype():
     def getVectors(self):
         return self.vectors
     
-    def getImages(self, k=None):
+    def getClassVectors(self):
+        return self.classVectors
+    
+    def setClassVectors(self, k=None):
+        # Normalizes random k vectors and sets classVectors
         if k is None:
             k = self.k
-        ''' TODO: Extract k images and change class vectors '''
+        random.seed(self.seed)
+        indices = random.sample(range(len(images)), k)
+        indices = torch.tensor(indices)
+        self.classVectors = normalize(self.vectors[indices]).numpy()
+    
+    def getClassVector(self):
+        # Calculates and returns classVector
+        self.classVector = self.classVectors.mean(axis=0)
+        return self.classVector
+        
+        
+        
